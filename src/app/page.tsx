08c47f1780c0ca8b4a9a2b0a2432da313db23200
Christopher <ChristopherOhwavworua@transalliance.ng>
2026-03-25 14:52:18 +0100

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -91,7 +91,7 @@ const page = () => {
 			{/* FAQ section */}
 			<section className='flex w-full flex-col items-center pt-16 slg:px-16 text-center bg-white'>
 				<h3 className='font-semibold text-xl sm:text-2xl slg:text-4xl tracking-tighter'>
-					Frequently Asked Question
+					Frequently Asked
 				</h3>
 				<FaqAccordion />
 			</section>
